fd95ce7e1136b7773049cac112274a73953d5c0a
David Goemans <dgoemans@gmail.com>
2015-03-29 17:56:00 +0200

    - Phaser setup

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 /**
  * Created by David on 29-Mar-15.
  */
-
 requirejs.config({
     baseUrl: 'src',
     paths: {
-        pixi: '../lib/pixi',
-        class: '../lib/js.class.min',
-        proton: '../lib/proton.min'
-    },
+        Phaser: '../lib/phaser-arcade-physics.min'
+    }
+});
 
-    shim: {
-        'class' : {
-            exports: 'Class'
-        },
-        'proton' : {
-            exports: 'Proton'
-        }
+require(["Phaser"], function(Phaser){
+    var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+    function preload()
+    {
+
+    }
+
+    function create()
+    {
+
+    }
+
+    function update()
+    {
 
     }
 });
